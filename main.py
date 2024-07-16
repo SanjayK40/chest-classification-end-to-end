fdb11030d4d6098a08ba7443d5917a0b5a550ad7
Sanjay <dennismob08@gmail.com>
2024-07-17 01:17:01 +0530

    deployment stage

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ except Exception as e:
 
 STAGE_NAME = "Evaluation stage"
 try:
-   logger.info(f"*******************")
+   logger.info(f"******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_evalution = EvaluationPipeline()
    model_evalution.main()
